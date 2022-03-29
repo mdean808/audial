cbc43a79b325a294ec5b634aaf129afcf8918078
Morgan Dean <morgandeanhi@gmail.com>
2022-03-29 03:39:07 -0700

    build and deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
@@ -12,7 +12,10 @@ const config = {
       pages: 'build',
       assets: 'build',
       fallback: null
-    })
+    }),
+    prerender: {
+      default: true
+    }
   }
 };
 
