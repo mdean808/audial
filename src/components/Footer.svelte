b648a76ceffcd233a9c8f4255fbe1403c8b8fb6e
Morgan Dean <morgandeanhi@gmail.com>
2022-04-12 14:01:42 -0700

    safari bug fix

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang='ts'>
   import { currentAttempt, currentSong, songPaused, temporaryAttempt } from '../store';
   import { onMount } from 'svelte';
   import type { Attempt } from '../types';
@@ -23,10 +23,12 @@
     player = new Audio(currentSong.get().preview);
     player.addEventListener('loadedmetadata', () => (songLength = player.duration));
     // disable media keys
-    navigator.mediaSession.setActionHandler('play', () => null);
-    navigator.mediaSession.setActionHandler('pause', () => null);
-    navigator.mediaSession.setActionHandler('seekbackward', null);
-    navigator.mediaSession.setActionHandler('seekforward', () => null);
+    if (navigator.mediaSession) { // handle weird safari-apple browser incompatibility
+      navigator.mediaSession.setActionHandler('play', () => null);
+      navigator.mediaSession.setActionHandler('pause', () => null);
+      navigator.mediaSession.setActionHandler('seekbackward', null);
+      navigator.mediaSession.setActionHandler('seekforward', () => null);
+    }
   });
 
   if (custom) temporaryAttempt.listen((value) => (attempt = value));
@@ -89,77 +91,77 @@
   };
 </script>
 
-<footer class="border-t border-white bottom-0 w-full fixed bg-gray-800">
-  <div class="border-b">
+<footer class='border-t border-white bottom-0 w-full fixed bg-gray-800'>
+  <div class='border-b'>
     <AttemptVisualizer {attempt} />
   </div>
 
-  <div class="max-w-screen-md mx-auto p-3">
+  <div class='max-w-screen-md mx-auto p-3'>
     <!-- TRACK CONTROL AREA -->
-    <div class="flex justify-evenly items-center w-full">
+    <div class='flex justify-evenly items-center w-full'>
       {#if player}
-        <div class="flex flex-1 justify-start font-mono">{timeElapsed}</div>
+        <div class='flex flex-1 justify-start font-mono'>{timeElapsed}</div>
       {/if}
-      <div class="text-center flex-1 justify-center">
+      <div class='text-center flex-1 justify-center'>
         <button
-          title="Play Song"
+          title='Play Song'
           class={`hover:text-blue-600 transition-colors duration-200 ${paused ? '' : 'hidden'}`}
           on:click={playSong}
         >
           <svg
-            class="w-14 h-14 mx-auto"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+            class='w-14 h-14 mx-auto'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z'
             />
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
             />
           </svg>
         </button>
         <button
-          title="Pause Song"
+          title='Pause Song'
           class={`hover:text-blue-600 transition-colors duration-200 ${paused ? 'hidden' : ''}`}
           on:click={stopSong}
         >
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-14 h-14 mx-auto"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
+            xmlns='http://www.w3.org/2000/svg'
+            class='w-14 h-14 mx-auto'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            stroke-width='2'
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
             />
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z"
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z'
             />
           </svg>
         </button>
       </div>
       {#if player}
-        <div class="flex flex-1 justify-end font-mono">
+        <div class='flex flex-1 justify-end font-mono'>
           {Math.floor(songLength / 60) +
-            ':' +
-            (Math.round(songLength % 60) < 10
-              ? '0' + Math.round(songLength % 60)
-              : Math.round(songLength % 60))}
+          ':' +
+          (Math.round(songLength % 60) < 10
+            ? '0' + Math.round(songLength % 60)
+            : Math.round(songLength % 60))}
         </div>
       {/if}
     </div>
