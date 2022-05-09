6b2312245e7c33ae2ebc9dac9686f1228a7cfd67
Morgan Dean <morgandeanhi@gmail.com>
2022-05-09 10:32:43 -1000

    build

---
+++ docs/service-worker.js
@@ -0,0 +1,12 @@
+const version = "v0.0.9";
+self.addEventListener("install", function() {
+  console.log("Uninstalling Service Worker in favor of version " + version);
+  self.skipWaiting();
+});
+self.addEventListener("activate", function() {
+  self.registration.unregister().then(function() {
+    return self.clients.matchAll();
+  }).then(function(clients) {
+    clients.forEach((client) => client.navigate(client.url));
+  });
+});
